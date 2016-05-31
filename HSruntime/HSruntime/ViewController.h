8c94cb7cbcfeb834a705fd6229f3c368d0f6b1a8
hansen <hansen@hansen.local>
2016-05-31 10:30:32 +0800

    初始化rumtime项目

---
+++ HSruntime/HSruntime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HSruntime
+//
+//  Created by hansen on 16/5/31.
+//  Copyright © 2016年 hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HSruntime/HSruntimeTests/HSruntimeTests.m
@@ -0,0 +1,39 @@
+//
+//  HSruntimeTests.m
+//  HSruntimeTests
+//
+//  Created by hansen on 16/5/31.
+//  Copyright © 2016年 hansen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HSruntimeTests : XCTestCase
+
+@end
+
+@implementation HSruntimeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
